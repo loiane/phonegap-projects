4d9f0d20d063211b34ff475270f6bc274818a6fc
Loiane Groner <loianeg@gmail.com>
2012-11-12 12:42:34 -0200

    first commit

---
+++ SenchaTouchPhoneGapiOS/app.js
@@ -0,0 +1,72 @@
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'touch/src',
+    'SenchaTouchPhoneGapiOS': 'app'
+});
+//</debug>
+
+Ext.application({
+    name: 'SenchaTouchPhoneGapiOS',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    views: ['Main'],
+
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    isIconPrecomposed: true,
+
+    startupImage: {
+        '320x460': 'resources/startup/320x460.jpg',
+        '640x920': 'resources/startup/640x920.png',
+        '768x1004': 'resources/startup/768x1004.png',
+        '748x1024': 'resources/startup/748x1024.png',
+        '1536x2008': 'resources/startup/1536x2008.png',
+        '1496x2048': 'resources/startup/1496x2048.png'
+    },
+
+    alertDismissed: function() {
+        // do something
+    },
+    
+    // Show a custom alertDismissed
+    //
+    showAlert: function () {
+        navigator.notification.alert(
+            'PhoneGap + Sencha Touch!',  // message
+            this.alertDismissed,         // callback
+            'It Works!',            // title
+            'OK'                  // buttonName
+        );
+    },
+
+    launch: function() {
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('SenchaTouchPhoneGapiOS.view.Main'));
+
+        this.showAlert();
+
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function(buttonId) {
+                if (buttonId === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    }
+});
